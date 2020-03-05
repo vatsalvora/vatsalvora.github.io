d51e52529389c0e45cf7ed3646be85e05b216259
Vatsal Vora <vatsalvora@Vatsals-Mini.home>
2020-03-05 11:31:35 -0500

    Updates

---
+++ precache-manifest.c120717bcf52e931e7d8bd74ef68938b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3979c0f883ee265ba96638100f93acf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf4cf6cb2d9beaa05914",
+    "url": "/static/js/2.3e4d93ad.chunk.js"
+  },
+  {
+    "revision": "85843481f5a03e14d6c3e7114b01a9e3",
+    "url": "/static/js/2.3e4d93ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e76f8f18922aa2d35d3",
+    "url": "/static/js/main.892b0256.chunk.js"
+  },
+  {
+    "revision": "366ddaea945e197457a1",
+    "url": "/static/js/runtime-main.ad264d5c.js"
+  }
+]);
